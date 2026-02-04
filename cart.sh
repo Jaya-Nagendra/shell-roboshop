085b54f754892ce087a0a8eab7296e1c93b3fd8f
Jaya-Nagendra <nagendra.44@outlook.com>
2026-02-04 10:51:46 +0530

    shell-roboshop

---
+++ cart.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+LOG_FOLDER="/var/log/roboshop"
+LOG_FILE="$LOG_FOLDER/$0.log"
+
+N="\e[0m"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+
+SCRIPT_DIR=$PWD
+
+USER_ID=$(id -u)
+
+if [ $USER_ID -ne 0 ]; then
+echo -e "$R Run this script with Root account $N" | tee -a $LOG_FILE
+exit 1
+fi
+
+mkdir -p $LOG_FOLDER
+
+VALIDATE(){
+    if [ $1 -eq 0 ]; then
+        echo -e "$2 ....$G SUCCESS $N" | tee -a $LOG_FILE
+        else
+        echo -e "$2 .... $R FAILED $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+dnf module disable nodejs -y
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+
+dnf install nodejs -y &>>$LOG_FILE
+VALIDATE $? "Install nodejs"
+
+mkdir -p /app 
+
+
+curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip
+
+VALIDATE $? "Download code"
+
+rm -rf /app/*
+
+cd /app 
+unzip /tmp/cart.zip
+VALIDATE $? "Unzip cart"
+
+cd /app 
+npm install 
+VALIDATE $? "npm install"
+
+cp $SCRIPT_DIR/cart.service /etc/systemd/system/cart.service
+VALIDATE $? "Copy systemctl service"
+
+systemctl daemon-reload &>>$LOG_FILE
+VALIDATE $? "Creload cart"
+
+systemctl enable cart 
+systemctl start cart &>>$LOG_FILE
+VALIDATE $? "Start cart"
